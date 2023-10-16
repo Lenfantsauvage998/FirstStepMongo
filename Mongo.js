d1317eeda7574aa226cfb013c20b3666eb6b449f
lenfantsauvage998 <lefantsauvage998@gmail.com>
2023-10-16 14:00:46 -0500

    Mongo

--- Mongo.js
+++ Mongo.js
@@ -1,6 +1,6 @@
 1. use Personas 
 Show collections 
-db.createCollections ("personas",{})
+db.createCollection ("personas",{})
 db.personas.insertOne({ nombre: 'Juan Perez', edad: 30, cuidad: 'Nueva York', })
 db.personas.insertOne({ nombre: 'Maria Gonzales', edad: 35, cuidad: 'Los Angeles',}) 
 db.personas.insertOne({ nombre: 'Carlos Ramirez', edad: 28, cuidad: 'Nueva York',})
@@ -11,4 +11,4 @@ db.personas.insertOne({ nombre: 'Carlos Ramirez', edad: 28, cuidad: 'Nueva York'
 
 4.db.personas.updateOne({ nombre: 'Juan Perez' }, {$set: {edad:31}})
 
-5.db.personas.updateMany({$and: [{ edad: { $gte: 25 } },{ cuidad: 'Nueva York' }]},{ $set: { cuidad: 'Los Angeles' } }) 
+5.db.personas.updateMany({ edad:{$gte:25, $lte:31}} , {$set:{cuidad: 'Nueva York'}})
